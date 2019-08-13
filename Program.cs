b6eca051caaac51a64a67314d6bb5dbee82df4d3
meliazeng <44716451+meliazeng@users.noreply.github.com>
2019-08-13 16:25:12 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace receiverApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World Test!");
         }
     }
 }
