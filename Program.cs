232ae390e195c4710ea8d4ecce96060f98ef0816
Your Name <cgjames@hotmail.com>
2019-08-13 16:10:47 +0000

    Init Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace receiverApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
